c075e6c1c0af15cb42d85558bb7d993a77604b1b
shubhangid3191 <shubhangid2004@gmail.com>
2026-09-16 13:05:06 +0530

    Create Biller folders

---
+++ frontend/src/pages/AddNewPatient.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function AddNewPatient() {
+  return (
+    <div>AddNewPatient</div>
+  )
+}
+
+export default AddNewPatient
---
+++ frontend/src/pages/AllPatient.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function AllPatient() {
+  return (
+    <div>AllPatient</div>
+  )
+}
+
+export default AllPatient
