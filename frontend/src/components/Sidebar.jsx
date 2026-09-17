bba8cfadf2a84501343a0d69a49fabe0634545a7
shubhangid3191 <shubhangid2004@gmail.com>
2026-09-17 13:22:54 +0530

    Update assets and navigation icons

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -16,7 +16,7 @@ import {
 } from "@mui/material";
 import { Add, Remove } from "@mui/icons-material";
 import logo from "../Assets/logo.png";
-import { HandIcon, ListCheckIcon } from "../Assets/Icons";
+import { HandIcon, ListCheckIcon } from "../Assets/Assets";
 
 const drawerWidth = 240;
 
--- frontend/src/components/TopBar.jsx
+++ frontend/src/components/TopBar.jsx
@@ -8,7 +8,7 @@ import {
   Typography,
 } from '@mui/material';
 import { alpha } from '@mui/material/styles';
-import { StarIcon, EditIcon, NotificationIcon } from '../Assets/Icons';
+import { StarIcon, EditIcon, NotificationIcon } from '../Assets/Assets';
 
 const drawerWidth = 240;
 
